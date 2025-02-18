3b219b772c4c5a8effa6b77d618cc7bc3f935d6d
Gianni Vullo <gian.vullo@gmail.com>
2025-02-18 14:24:01 +0100

    fixed problem with the +SP not working

--- src/routes/dashboard/patients/[patientId]/+page.svelte
+++ src/routes/dashboard/patients/[patientId]/+page.svelte
@@ -22,11 +22,10 @@
 <Title patient={data.patient} />
 {#if sps}
 	<div
-		class="-z-50 mt-10 w-full border-b border-gray-200 pb-5 sm:flex sm:items-center sm:justify-between">
+		class="mt-10 w-full border-b border-gray-200 pb-5 sm:flex sm:items-center sm:justify-between">
 		<h3 class="text-base font-semibold text-gray-900">Situations pathologiques</h3>
 		<div class="mt-3 sm:ml-4 sm:mt-0">
 			<a
-			id="test"
 				href={`/dashboard/patients/${data.patient.patient_id}/situation-pathologique/create`}
 				class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 				>Ajouter une situation pathologique</a>
