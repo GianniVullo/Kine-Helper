1b9fc41576ffda123dbfac105cc663d1ea7811fe
Gianni Vullo <gian.vullo@gmail.com>
2025-02-18 14:32:09 +0100

    added error propagation and handling

--- src/routes/dashboard/patients/+page.js
+++ src/routes/dashboard/patients/+page.js
@@ -1,12 +1,16 @@
 import { trace } from '@tauri-apps/plugin-log';
 import { appState } from '../../../lib/managers/AppState.svelte';
 import { listPatients } from '../../../lib/user-ops-handlers/patients';
+import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
 export async function load() {
 	trace('Loading patients from local db');
-	await appState.init({})
-	let patients = await listPatients(appState.user);
+	await appState.init({});
+	let { data: patients, error } = await listPatients(appState.user);
+	if (error) {
+		error(500, { message: error });
+	}
 	console.log(patients);
 
 	trace('Patients loaded from local db');
--- src/routes/dashboard/patients/[patientId]/+layout.js
+++ src/routes/dashboard/patients/[patientId]/+layout.js
@@ -2,19 +2,27 @@ import { retrievePatient } from '../../../../lib/user-ops-handlers/patients';
 import { appState } from '../../../../lib/managers/AppState.svelte.js';
 import { trace } from '@tauri-apps/plugin-log';
 import { retrieveSituationPathologique } from '../../../../lib/user-ops-handlers/situations_pathologiques.js';
+import { error } from '@sveltejs/kit';
 
 export async function load({ params, depends }) {
 	depends('patient:layout');
 	trace('Entering load function, engaging patient retrival');
 	await appState.init({});
-	let patient = await retrievePatient({ patient_id: params.patientId });
+	let { data: patient, error } = await retrievePatient({ patient_id: params.patientId });
+	if (error) {
+		error(500, { message: error.message });
+	}
 	if (!patient) {
 		patient = 'none';
 	}
 
 	let sp;
 	if (params.spId) {
-		sp = await retrieveSituationPathologique({ sp_id: params.spId });
+		const { data, error } = await retrieveSituationPathologique({ sp_id: params.spId });
+		if (error) {
+			error(500, { message: error.message });
+		}
+		sp = data;
 		if (!sp) {
 			sp = 'none';
 		}
