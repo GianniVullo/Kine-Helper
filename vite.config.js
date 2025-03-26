8e5683245e396066349793aca7e0183584c336c3
Gianni Vullo <gian.vullo@gmail.com>
2025-03-26 21:37:34 +0100

    Correction prélancement 7

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import purgeCss from 'vite-plugin-tailwind-purgecss';
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-	plugins: [sveltekit(), purgeCss()],
+	plugins: [sveltekit()],
 
 	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
 	//
