8f4be423a9a1213023928a40c83c96e6b4fc1ab8
Gianni Vullo <gian.vullo@gmail.com>
2025-04-13 20:56:45 +0200

    ajout d'un effet de transition onNavigate

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,6 +18,19 @@
 		});
 		document.dispatchEvent(myEvent);
 	});
+
+	import { onNavigate } from '$app/navigation';
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 <Notification />
