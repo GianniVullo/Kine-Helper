996c89ad0062bbe5783282f84c008b7e8d01aaf8
Gianni Vullo <gian.vullo@gmail.com>
2025-04-17 19:19:00 +0200

    added package visualizer

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
-
-// @ts-expect-error process is a nodejs global
+import { visualizer } from 'rollup-plugin-visualizer'; // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [
+		visualizer({
+			emitFile: true,
+			filename: 'stats.html'
+		}),
+		tailwindcss(),
+		sveltekit()
+	],
 
 	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
 	//
