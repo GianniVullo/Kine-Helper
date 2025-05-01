31b60c126054cb51ba2349e010965591dbdfe5ba
Gianni Vullo <gian.vullo@gmail.com>
2025-05-02 01:20:16 +0200

    setup the create multiple seance page

---
+++ src/routes/dashboard/patients/[patientId]/situation-pathologique/[spId]/seances/create-multiple/+page.js
@@ -0,0 +1,14 @@
+import { appState } from '../../../../../../../../lib/managers/AppState.svelte';
+import { gatherTarifsforpageLoad } from '../../../../../../../../lib/cloud/components/forms/tarification-fields/tarifHelpers.js';
+
+export const load = async ({ params }) => {
+	if (!appState.db) {
+		await appState.init({});
+	}
+	return await gatherTarifsforpageLoad(params.spId);
+};
+
+/** @type {import('./$types').EntryGenerator} */
+export function entries() {
+	return [{ patientId: 'test-patient', spId: '0b017e35-2b9a-4462-8723-fa2740af5ca2' }];
+}
---
+++ src/routes/dashboard/patients/[patientId]/situation-pathologique/[spId]/seances/create-multiple/+page@[patientId].svelte
@@ -0,0 +1,9 @@
+<script>
+	import MultipleSeanceForm from '../../../../../../../../lib/cloud/components/forms/seances/MultipleSeanceForm.svelte';
+
+	/** @type {{ data: import('./$types').PageData }} */
+	let { data } = $props();
+	let { patient, sp, tarifs, supplements, prescriptions } = data;
+</script>
+
+<MultipleSeanceForm {patient} {sp} {tarifs} {supplements} {prescriptions} />
