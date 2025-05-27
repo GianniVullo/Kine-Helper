133c41870492d653c9ff84a480ca93e5df45ede4
Gianni Vullo <gian.vullo@gmail.com>
2025-05-27 16:52:12 +0200

    fixed the bottomRightControlleBar. It did not display even when operations where running

--- src/lib/cloud/libraries/History/BottomRightControlBar.svelte
+++ src/lib/cloud/libraries/History/BottomRightControlBar.svelte
@@ -2,6 +2,7 @@
 	import BottomLayout from './BottomLayout.svelte';
 	import QueueStateDetailModal from './QueueStateDetailModal.svelte';
 	import { appState } from '../../../managers/AppState.svelte';
+	// import TestingController from './TestingController.svelte';
 
 	let historyManager = appState.queue;
 </script>
@@ -12,7 +13,7 @@
 
 	<QueueStateDetailModal {historyManager} />
 
-	{#if historyManager.promiseQueue.length > 0}
+	{#if historyManager.promiseQueue.operations.length > 0}
 		<div class="fixed right-0 bottom-0 h-8 rounded-tl-lg bg-black/80 px-2 py-1 text-white md:w-96 z-50">
 			<div class="flex items-center text-sm">
 				{#if historyManager.promiseQueue.running}
