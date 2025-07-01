fec1aa5e0488644b4c800147ca71869ab9a3b278
Gianni Vullo <gian.vullo@gmail.com>
2025-07-01 15:42:46 +0200

    Added the number of séance in the Seances pages in parenthesis so the user can now quickly how much seance he did in the SP he's checking

--- src/routes/dashboard/patients/[patientId]/situation-pathologique/[spId]/seances/+page.svelte
+++ src/routes/dashboard/patients/[patientId]/situation-pathologique/[spId]/seances/+page.svelte
@@ -1,14 +1,11 @@
 <script>
-	import { t } from '../../../../../../../lib/i18n';
+	// import { t } from '../../../../../../../lib/i18n';
 	import dayjs from 'dayjs';
 	import SectionTitle from '../../../../../../../lib/components/SectionTitle.svelte';
 	import BoutonSecondaireAvecIcone from '../../../../../../../lib/components/BoutonSecondaireAvecIcone.svelte';
-	import { addIcon, successIcon } from '../../../../../../../lib/ui/svgs/IconSnippets.svelte';
+	import { addIcon} from '../../../../../../../lib/ui/svgs/IconSnippets.svelte';
 	import BoutonPrincipalAvecIcone from '../../../../../../../lib/components/BoutonPrincipalAvecIcone.svelte';
-	import Select from '../../../../../../../lib/components/form/Select.svelte';
 	import EventCalendar from '../../../../../../../lib/EventCalendar.svelte';
-	import CardTable from '../../../../../../../lib/components/CardTable.svelte';
-	import { toast } from '../../../../../../../lib/cloud/libraries/overlays/notificationUtilities.svelte';
 	import CalendarEventModal from '../../../../../../../lib/ui/CalendarEventModal.svelte';
 	import { openDrawer } from '../../../../../../../lib/cloud/libraries/overlays/drawerUtilities.svelte';
 	import { page } from '$app/state';
@@ -42,7 +39,7 @@
 	});
 
 	let display = $state('calendar');
-	let ec =$state();
+	let ec = $state();
 </script>
 
 <Drawer
@@ -52,7 +49,7 @@
 	<CalendarEventModal event={page.state.drawer?.event} seance={page.state.drawer?.seance} {ec} />
 </Drawer>
 
-<SectionTitle titre="Séances" className="space-x-2">
+<SectionTitle titre={`Séances (${sp?.seances?.length})`} className="space-x-2">
 	{#snippet actions()}
 		<!-- <div class="flex items-center space-x-2">
 			<Select id="affichage" bind:value={display}>
@@ -64,13 +61,11 @@
 		</div> -->
 		<!-- TODO : Remettre ça une fois que ce sera ergonomique -->
 		<BoutonSecondaireAvecIcone
-
 			href={`/dashboard/patients/${patient.patient_id}/situation-pathologique/${sp.sp_id}/seances/create-multiple`}
 			inner={'Séances multiples'}
 			icon={addIcon} />
 		<BoutonPrincipalAvecIcone
 			href={`/dashboard/patients/${patient.patient_id}/situation-pathologique/${sp.sp_id}/seances/create`}
-
 			className="ml-3 inline-flex items-center bg-indigo-600 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 			inner="Séance"
 			icon={addIcon} />
