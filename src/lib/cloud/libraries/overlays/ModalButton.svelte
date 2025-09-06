dd8b15fc20d4773aa1ef81101aee84e07817eb65
Gianni Vullo <gian.vullo@gmail.com>
2025-09-06 09:24:59 +0200

    Added new tailwind plus Element modal

---
+++ src/lib/cloud/libraries/overlays/ModalButton.svelte
@@ -0,0 +1,5 @@
+<button
+	command="show-modal"
+	commandfor="dialog"
+	class="rounded-md bg-gray-950/5 px-2.5 py-1.5 text-sm font-semibold text-gray-900 hover:bg-gray-950/10"
+	>Open dialog</button>
