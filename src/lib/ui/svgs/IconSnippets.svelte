05734c54d7bfbf63e79133e7d01c037c9b4ff349
Gianni Vullo <gian.vullo@gmail.com>
2025-09-24 16:48:14 +0200

    Added new icons

--- src/lib/ui/svgs/IconSnippets.svelte
+++ src/lib/ui/svgs/IconSnippets.svelte
@@ -42,9 +42,28 @@
 		homeIcon,
 		userMultipleIcon,
 		userIdIcon,
-		clipBoardDocIcon
+		clipBoardDocIcon,
+		burgerIcon,
+		boueeIcon
 	};
 </script>
+
+
+{#snippet burgerIcon(cls)}
+	<svg
+		class={cls}
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		aria-hidden="true"
+		data-slot="icon">
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+	</svg>
+{/snippet}
 
 {#snippet bookIcon(cls)}
 	<svg
@@ -116,6 +135,20 @@
 {/snippet}
 
 {#snippet magnifyingGlassIcon(cls)}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class={cls}>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+	</svg>
+{/snippet}
+{#snippet boueeIcon(cls)}
 	<svg
 		class={cls}
 		fill="none"
