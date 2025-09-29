8a9d3b12e722e93f4ee0c3ad6410728ccbbdae44
Gianni Vullo <gian.vullo@gmail.com>
2025-09-29 15:33:03 +0200

    Updated an unknow git error I induced by typing unsafe commands from Claude AI. Lesson learned now...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
 	import { platform } from '@tauri-apps/plugin-os';
 	import { goto } from '$app/navigation';
 	import logoUrl from '$lib/assets/logo head.avif';
+	import { invoke } from '@tauri-apps/api/core';
 	// import RichTextEditor from '../lib/cloud/libraries/rich-text-editor/RichTextEditor.svelte';
 	// import TemplateCreatorModal from '../lib/cloud/libraries/rich-text-editor/TemplateCreatorModal.svelte';
 	// `windowed: true` actually sets the webview to transparent
@@ -40,19 +41,12 @@
 		<div class="flex flex-col items-center justify-center">
 			<img src={logoUrl} alt="Your Company" class="mx-auto h-40 w-auto" />
 		</div>
-		<h2 class="mt-5 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-			{$t('login', 'title', {}, 'Sign in to your account')}
-		</h2>
-	</div>
-
-	<div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
 		<LoginForm {message} />
-		<!--? We remove the ability to sign up in the app and we will only allow it on the web. this is to enforce the appStore policies and avoid issues with the app store review process. -->
 		<button
 			onclick={async () => {
 				await open('https://kine-helper.be/reset-password');
 			}}
-			class="mt-10 w-full text-center text-sm/6 text-gray-500">
+			class="text-gray-600">
 			{$t('login', 'controls.forgot')}</button>
 	</div>
 </div>
---
+++ src/routes/test/ui-testing/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import AppLayout from '../../../lib/components/app-layout/AppLayout.svelte';
+
+	/** @type {{ data: import('./$types').PageData }} */
+	let { data } = $props();
+</script>
+
+<AppLayout>
+    BONJOUR !
+</AppLayout>
